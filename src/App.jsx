9eb659a0ef77c7103d73d197963b110527b4e40c
KARIYER\saim.ankarali <akbeta08@gmail.com>
2022-02-12 17:12:12 +0300

    feat: list components in page for example

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './styles/app.css'
+import Card from './components/Card'
+import Widget from './components/Widget'
+import WorkSvg from '../images/icon-work.svg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +10,8 @@ function App() {
   return (
     <div className="App">
       <main>
-        <p>main</p>
+        <Card title="John Doe" profilePic="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200" />
+        <Widget title="John Doe" current="10" previous="5" image={WorkSvg} />
       </main>
       <footer>
         <p>footer</p>
